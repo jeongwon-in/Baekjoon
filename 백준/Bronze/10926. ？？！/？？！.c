6de2ff631b88c7feef517b5ea97781114837f08f
jeongwon-in <155473443+jeongwon-in@users.noreply.github.com>
2026-03-18 23:16:22 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define SIZE(a) (sizeof(a)/sizeof((a)[0]))
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(void) {
+	char str[50];
+	scanf("%s", str);
+	printf("%s??!", str);
+
+	return 0;
+}
