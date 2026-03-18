e3471da7efeee82670baa251231d483521d254c4
jeongwon-in <155473443+jeongwon-in@users.noreply.github.com>
2026-03-18 23:11:55 +0900

    [Bronze V] Title: Hello World, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define SIZE(a) (sizeof(a)/sizeof((a)[0]))
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#if 01
+
+int main(void) {
+
+	printf("Hello World!");
+
+	return 0;
+}
+
+#endif // 01
