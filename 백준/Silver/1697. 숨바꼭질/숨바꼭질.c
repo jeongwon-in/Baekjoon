33040799632bf07043d829ecae2978c6a7a67957
jeongwon-in <155473443+jeongwon-in@users.noreply.github.com>
2026-03-30 22:34:11 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 1896 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.c
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_N (100000 + 1)
+
+int N, K;
+int subin[MAX_N];
+
+typedef struct _node {
+	int X;
+}node;
+
+node Queue[MAX_N];
+int front, rear;
+
+int solve(void) {
+
+	if (N == K) return 0;
+
+	int nX, i;
+	node curr;
+
+	front = rear = 0;
+	Queue[rear++] = (node){ N };
+	subin[N] = 1;
+
+	while (front != rear) {
+
+		curr = Queue[front++];
+
+		int dX[3] = { -1, 1, curr.X };
+		for (i = 0; i < 3; ++i) {
+			nX = curr.X + dX[i];
+
+			if (nX < 0 || nX > MAX_N) continue;
+
+			if (subin[nX] == 0) {
+				Queue[rear++] = (node){ nX };
+				subin[nX] = subin[curr.X] + 1;
+			}
+		}
+	}
+	return subin[K] - 1;
+}
+
+void inputData(void) {
+	scanf("%d %d", &N, &K);
+}
+
+int main(void) {
+
+	inputData();
+
+	printf("%d", solve());
+
+	return 0;
+}
